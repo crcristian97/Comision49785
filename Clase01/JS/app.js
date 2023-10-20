26b301266a6005676767d231d24f61ec3d0f0c17
crcristian97 <crcristian97.cc@gmail.com>
2023-10-20 19:08:42 -0300

    Nueva Comision@

---
+++ Clase01/JS/app.js
@@ -0,0 +1,82 @@
+//Sirve para chequear que se conecto mi archivo JS
+console.log("Conectado");
+
+//Reglas basicas para comentar 
+
+// 1 forma de comentar codigo es con // Comentario de Linea
+/*
+console.log("Conectado");
+console.log("Conectado");
+console.log("Conectado");
+console.log("Conectado");
+console.log("Conectado");
+console.log("Conectado");
+console.log("Conectado");
+Comentario de bloque
+*/
+
+//Palabras reservadas
+//Existen variables que guardan informacion
+//3 tipos de variables
+
+//1era: 
+//var edad = 18; //NO LA USAN NUNCA EN SU VIDA, SI LA TIENE PUNTO MENOS
+//ESMC 5
+
+//2da forma:
+//let nombreUsuario = "Cristian Cabrera";
+
+//3era forma:
+const CURSO = "Javascript";
+
+//Declaracion de variable: significa crearla
+//correcto
+//let apellido;
+let telefono;
+//const anioNacimiento;
+//incorrecto
+var año
+var teléfonoMóvil
+var teléfono_móvil
+let declarionAnonymous
+
+//Tipos de valores
+//const IVA = 0.21; //valores numerico
+const edad = 18
+
+//valor string
+let nombreUsuario = "Cristian Cabrera";
+let apellidoUsuario = 'Cracken';
+
+//valor boolean
+let esMayorEdad = true;
+let esActivo = false;
+
+//valor undefined
+//Reprensenta un valor no definido
+let apellido;
+let apellido2 = undefined;
+
+//valor null
+//Reprenseta la ausencia de un valor
+let valorNulo = null;
+
+//Operaciones basicas
+let numeroA = 50;
+let numeroB = 70;
+const IVA = 0.21;
+
+//Sumamos numeros
+//CONCATENACION
+let resultadoSuma = numeroA + numeroB;
+console.log(resultadoSuma);
+
+let resultadoResta = numeroA - numeroB;
+console.log(resultadoResta);
+
+let resultadoProducto = numeroA * IVA;
+console.log(resultadoProducto)
+
+
+
+
